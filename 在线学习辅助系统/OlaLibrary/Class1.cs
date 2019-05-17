89696684c9af2ce99163d28e956a180288fab997
cwn <2840083875@qq.com>
2019-05-17 19:22:04 +0800

    ver1.0.1 分四个文件夹：OlaLibrary项目编写功能类库，Test项目负责测试代码，在线学习辅助系统项目负责编写主窗体，package文件夹保存了其他引用的非系统类库。

---
+++ 在线学习辅助系统/OlaLibrary/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace OlaLibrary
+{
+    /// <summary>
+    /// 表示MySql存储过程或存储函数的一个参数。
+    /// </summary>
+    public sealed class StoredParameter
+    {
+        public string Name { get; private set; }
+        public MySqlDbType Type { get; private set; }
+        public ParameterDirection Direction { get; private set; }
+
+        /// <summary>
+        /// 构建一个存储参数的唯一途径。
+        /// </summary>
+        /// <param name="name">创建存储过程时指定的参数名</param>
+        /// <param name="type">参数类型</param>
+        /// <param name="direction">输入参数/输出参数/输入输出参数</param>
+        public StoredParameter(string name, MySqlDbType type, ParameterDirection direction)
+        {
+            Name = name;
+            Type = type;
+            Direction = direction;
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// 与MySql交互的接口。
+    /// </summary>
+    public sealed class DatabaseService
+    {
+        MySqlConnection con;
+        MySqlCommand cmd;
+
+        /// <summary>
+        /// 初始化MySql通信接口。
+        /// </summary>
+        public DatabaseService()
+        {
+
+        }
+
+        public CreateUser(string userName, string password,)
+    }
+}
---
+++ 在线学习辅助系统/Test/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //创业基础考试
+            string s = "server=127.0.0.1;port=3306;user=root;password=testuse;database=learning_mysql;";
+            MySqlConnection conn = new MySqlConnection(s);
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            conn.ChangeDatabase("mysql");
+            cmd.CommandText = "create user xyz identified by '123456'";
+            cmd.ExecuteNonQuery();
+
+            //cmd.CommandText = "p1";
+            //cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.Parameters.Add("@o", MySqlDbType.Int32);
+            //cmd.Parameters["@o"].Direction = ParameterDirection.Output;
+            //cmd.ExecuteNonQuery();
+            //var x = cmd.Parameters;
+            //Console.WriteLine(x["@o"].Value.ToString());
+            conn.Close();
+            
+        }
+    }
+
+    /// <summary>
+    /// 表示MySql存储过程或存储函数的参数，是对MySqlParameter的进一步封装。
+    /// </summary>
+    public sealed class StoredParameter
+    {
+        public string Name { get; private set; }
+        public MySqlDbType Type { get; private set; }
+        public ParameterDirection Direction { get; private set; }
+
+        /// <summary>
+        /// 构建一个参数的唯一途径。
+        /// </summary>
+        /// <param name="name">创建存储过程时指定的参数名</param>
+        /// <param name="type">参数类型</param>
+        /// <param name="direction">输入参数/输出参数/输入输出参数</param>
+        public StoredParameter(string name, MySqlDbType type, ParameterDirection direction)
+        {
+            Name = name;
+            Type = type;
+            Direction = direction;
+        }
+    }
+}
